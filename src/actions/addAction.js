dce8d45c6b8a249406e8029cc1ce285d9acba3df
Kamal <geniusamansingh@gmail.com>
2022-07-22 21:29:45 +0530

    go image uploaded

---
+++ src/actions/addAction.js
@@ -0,0 +1,14 @@
+import { ADD_PRODUCT_BASKET } from './types';
+
+export const addBasket = (productName) => {
+    return (dispatch) => {
+        console.log("Adding to Basket");
+        console.log("Product: ", productName);
+        dispatch({
+            type: ADD_PRODUCT_BASKET,
+            payload: productName
+        });
+    }
+}
+
+//dispatch the type of action
---
+++ src/components/Home.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import greyTshirt from '../images/greytshirt.jpg';
+import react from '../images/react.jpg';
+import go from '../images/go.png';
+import { connect } from 'react-redux';
+import { addBasket } from '../actions/addAction';
+import typescript from '../images/ts.jpg';
+import hcv from '../images/hcv.jpg';
+import rust from '../images/rust.jpg';
+
+const Home = (props) => {
+    console.log('home props',props);
+    return(
+        <div className="container">
+            
+            <div className="image">
+                <img src={rust} alt="rust" />
+                <h3>Rust</h3>
+                <h3>$21,00</h3>
+                <a onClick={() => props.addBasket('rust')} className="addToCart cart1" href="#">Add to Cart</a>
+            </div>
+            <div className="image">
+                <img src={go} alt="go" />
+                <h3>Go</h3>
+                <h3>$35,00</h3>
+                <a onClick={() => props.addBasket('go')} className="addToCart cart2" href="#">Add to Cart</a>
+            </div>
+            <div className="image">
+                <img src={typescript} alt="Black Hoddie" />
+                <h3>Typescript</h3>
+                <h3>$15,00</h3>
+                <a onClick={() => props.addBasket('typescript')} className="addToCart cart3" href="#">Add to Cart</a>
+            </div>
+            <div className="image">
+                <img src={hcv} alt="Black Hoddie" />
+                <h3>hcv</h3>
+                <h3>$5,00</h3>
+                <a onClick={() => props.addBasket('hcv')} className="addToCart cart4" href="#">Add to Cart</a>
+            </div>
+            <div className="image">
+                <img src={react} alt="Grey Hoddie" classname="resize"/>
+                <h3>ReactJS</h3>
+                <h3>$120,00</h3>
+                <a onClick={() => props.addBasket('react')} className="addToCart cart1" href="#">Add to Cart</a>
+            </div>
+        </div>
+    );
+}
+
+export default connect(null, { addBasket })(Home);
