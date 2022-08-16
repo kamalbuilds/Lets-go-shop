20095211f87bdc6f9683a5bb8a80181ea9ea3591
Kamal Nayan <geniusamansingh@gmail.com>
2022-08-16 18:59:11 +0000

    material-ui button for cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import { connect } from 'react-redux';
 import { getNumbers } from '../actions/getAction';
 import { Link } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 
 function Navbar(props) {
     console.log('Navbar',props);
@@ -16,13 +17,16 @@ function Navbar(props) {
             <nav>
             <h2>Let's Shop 💝</h2>
             <ul>
-                <li><Link to="/">Home</Link></li>
+                <Button variant="contained" color="success"><Link to="/">Home</Link></Button>
                 <li><Link to="/about">About</Link></li>
                 <li className="cart">
-                <Link to="/cart">
-                <ion-icon name="basket"></ion-icon>Cart
-                <span>{props.basketProps.basketNumbers}</span>
-                </Link>
+                
+                <Button variant="outlined" color="primary">
+                    <Link to="/cart"><ion-icon name="basket"></ion-icon>Cart
+                    <span>{props.basketProps.basketNumbers}</span>
+                    </Link>
+                </Button>
+                
                 </li>
             </ul> 
             </nav>
