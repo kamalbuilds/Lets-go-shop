57763868767d1f50e4675275513b021ec41da25b
unknown <muhammadirfan9273@gmail.com>
2022-08-17 00:24:52 -0700

    material ui button added and align horizontally

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,66 @@
-import React, {useEffect} from 'react';
-import { connect } from 'react-redux';
-import { getNumbers } from '../actions/getAction';
-import { Link } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getNumbers } from "../actions/getAction";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
 
 function Navbar(props) {
-    console.log('Navbar',props);
 
-    useEffect(() => {
-        console.log(getNumbers());
-        getNumbers(); 
-    }, []);
-    return (
-        <header>
-            <div className="overlay"></div>
-            <nav>
-            <h2>Let's Shop 💝</h2>
-            <ul>
-                <Button variant="contained" color="success"><Link to="/">Home</Link></Button>
-                <li><Link to="/about">About</Link></li>
-                <li className="cart">
-                
-                <Button variant="outlined" color="primary">
-                    <Link to="/cart"><ion-icon name="basket"></ion-icon>Cart
-                    <span>{props.basketProps.basketNumbers}</span>
-                    </Link>
-                </Button>
-                
-                </li>
-            </ul> 
-            </nav>
-      </header>
-    );
+  console.log("Navbar", props);
+
+  useEffect(() => {
+    console.log(getNumbers());
+    getNumbers();
+  }, []);
+  return (
+    <header>
+      <div className="overlay"></div>
+      <nav>
+        <h2>Let's Shop 💝</h2>
+        <ul>
+          <li>
+            <Button
+              variant="contained"
+              sx={{ color: 'white', backgroundColor: 'orange' }}
+            >
+              <Link to="/">Home</Link>
+            </Button>
+          </li>
+          <li>
+            <Button
+              variant="contained"
+                sx={{ color: 'white', backgroundColor: 'orange' }}
+            >
+              <Link to="/about">About</Link>
+            </Button>
+          </li>
+          <li>
+            <Button
+              variant="contained"
+              startIcon={<ShoppingBasketIcon style={{color:"blue"}} />}
+              style={{
+                backgroundColor: "orange",
+                color: "red",
+                listStyle: "none",
+                textDecoration:"none"
+              }}
+            >
+              <Link to="/cart">
+                Cart
+                <span>{props.basketProps.basketNumbers}</span>
+              </Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-const mapStateToProps = state => ({
-    basketProps: state.basketState
-})
+const mapStateToProps = (state) => ({
+  basketProps: state.basketState,
+});
 
 export default connect(mapStateToProps, { getNumbers })(Navbar);
