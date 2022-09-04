f5d3cfe5da24030d3f41269f374b7d1217e19268
Tamal Das <gyansujan69@gmail.com>
2022-09-04 10:52:50 +0530

    fix : Added "add" text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Cart from './components/Cart';
-import './App.css';
-import { Provider } from 'react-redux';
-import store from './store.js'; //importing the store 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import About from './components/About';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store.js"; //importing the store
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/cart" component={Cart} />
-          <Route path="/about" component={About} />
-        </Switch>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/about" component={About} />
+          </Switch>
         </BrowserRouter>
       </div>
     </Provider>
