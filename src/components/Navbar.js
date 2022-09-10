0c652f7759de55dae274a4811ee5a47276ca1d36
Caleb <caleb.cyrill@gmail.com>
2022-09-10 03:33:48 -0500

    fixed the button issue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar(props) {
   }, []);
   return (
     <header>
-      <div className="overlay"></div>
+      <div className="overlay">
       <nav>
         <h2>Let's Shop 💝</h2>
         <ul>
@@ -55,7 +55,8 @@ function Navbar(props) {
             </Link>
           </li>
         </ul>
-      </nav>
+        </nav>
+        </div>
     </header>
   );
 }
